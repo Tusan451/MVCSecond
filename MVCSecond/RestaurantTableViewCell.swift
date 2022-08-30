59062799dd9e4783783ea9c8eb6a421b7d3dc6bc
Oleg Fedotov <trevor706@gmail.com>
2022-08-30 13:04:40 +0300

    RestaurantTableViewCell

---
+++ MVCSecond/RestaurantTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantTableViewCell.swift
+//  MVCSecond
+//
+//  Created by Olegio on 30.08.2022.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var restaurantImage: UIImageView!
+    @IBOutlet var restaurantName: UILabel!
+    @IBOutlet var restaurantRating: UILabel!
+    @IBOutlet var restaurantCost: UILabel!
+    @IBOutlet var restaurantAddress: UILabel!
+    
+}
